0699386ee7ea1363a75f90556801977c725fc47a
namanvashistha <namanvashistha15@gmail.com>
2018-10-11 17:04:15 +0530

    code isinserted into list of list

---
+++ com.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <list>
+using namespace std;
+
+int main(){
+    fstream file;
+    string filename="code.txt";
+    file.open("code.txt");
+    string s="";
+    while(file){
+
+        string p;
+        getline(file,p);
+        s+=p;
+    }
+    list<string> l;
+    list<list<string>> lol;
+    string word="";
+    for(unsigned int i=0;i< s.length()  ;i++){
+        if(s[i]!=' ' && s[i]!=';' ){
+            word+=s[i];
+        }
+        else{
+            if(word!=""){
+                l.push_back(word);
+            }
+            word="";
+        }
+        if(s[i]==';'){
+            lol.push_back(l);
+            l.erase(l.begin(),l.end());
+            i++;
+        }
+    }
+
+    list<list<string>>::iterator itr;
+    for (itr=lol.begin(); itr != lol.end(); itr++){
+        list<string>tl=*itr;
+        list<string>::iterator it;
+        for(it=tl.begin();it!=tl.end();it++){
+            cout<<*it<<"->";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
