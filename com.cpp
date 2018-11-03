ab88f3f8834614efc4b633a137ff500536c13636
namanvashistha <namanvashistha15@gmail.com>
2018-11-04 01:20:23 +0530

    addition of displaying fetching features

--- com.cpp
+++ com.cpp
@@ -1,49 +1,42 @@
 #include <iostream>
-#include <fstream>
+
 #include <string>
 #include <list>
 using namespace std;
 
-int main(){
-    fstream file;
-    string filename="code.txt";
-    file.open("code.txt");
-    string s="";
-    while(file){
-
-        string p;
-        getline(file,p);
-        s+=p;
-    }
+int main()
+{
     list<string> l;
-    list<list<string>> lol;
-    string word="";
+   list<list<string>> lol;
+   string word="";
+    string s="first line ; second line ; third line ;";
     for(unsigned int i=0;i< s.length()  ;i++){
-        if(s[i]!=' ' && s[i]!=';' ){
-            word+=s[i];
-        }
-        else{
-            if(word!=""){
-                l.push_back(word);
-            }
-            word="";
-        }
         if(s[i]==';'){
             lol.push_back(l);
             l.erase(l.begin(),l.end());
             i++;
+            }
+        if(s[i]!=' '){
+            word+=s[i];
+        }
+        else{
+            l.push_back(word);
+            word="";
         }
     }
-
     list<list<string>>::iterator itr;
     for (itr=lol.begin(); itr != lol.end(); itr++){
         list<string>tl=*itr;
         list<string>::iterator it;
         for(it=tl.begin();it!=tl.end();it++){
-            cout<<*it<<"->";
+            if(*it=="display"){
+                cout<<"yes";
+                next(it,1);
+                cout<<*it<<endl;
+            }
         }
-        cout<<endl;
     }
 
-    return 0;
+
+   return 0;
 }
